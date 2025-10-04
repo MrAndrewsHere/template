d769ba8a3606a4815c404fbbc6b23e6113be91b2
MrAndrewsHere <32438685+MrAndrewsHere@users.noreply.github.com>
2025-10-04 13:40:17 +0000

    auth

---
+++ app/Domain/Task/Controllers/TaskController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\Task\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class TaskController extends Controller
+{
+    public function index() {}
+
+    public function show() {}
+
+    public function store() {}
+
+    public function update() {}
+
+    public function destroy() {}
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,17 @@
 <?php
 
-\Illuminate\Support\Facades\Route::get('/', function () {
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
     return app()->version();
+});
+
+Route::prefix('v1')->middleware('auth:sanctum')->group(function ($route) {
+
+    $route->get('/', fn () => app()->version());
+
+    $route->get('/user', function (Request $request) {
+        return $request->user();
+    })->name('user');
 });
