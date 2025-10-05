117e81aeeaeca1f1b09985185c6d4fccfbe5c73e
MrAndrewsHere <32438685+MrAndrewsHere@users.noreply.github.com>
2025-10-05 19:27:44 +0000

    small

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -16,6 +16,9 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->resource->id,
+            'name' => $this->resource->name,
+        ];
     }
 }
